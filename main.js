f2cb34b82e8f4fcadf29d6382349068077d6c201
Diego Eiras <eiras.lucio@gmail.com>
2021-12-17 18:15:15 -0300

    Update retirados

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
 const generateButton = document.querySelector('#generate');
 const resultText = document.querySelector('#result');
 
+const retirados = [86, 90, 32, 29, 36, 92, 22, 40, 60, 98, 58, 45, 93, 31, 14, 
+  41, 87, 34, 19, 79, 8, 1, 35, 10, 21, 6, 79, 52, 69, 95, 57, 
+  60, 84, 71, 54, 70, 29, 97, 58, 79, 66, 58, 82, 44, 39, 21,
+  17, 67, 54, 59, 39 , 13, 71, 62, 27, 5, 74, 78, 97,18, 10]
+
 function generateNumbers() {
   const numbers = [];
 
   for (let i = 0; i < 100; i++) {
-    numbers.push(i);
+    if (!retirados.includes(i)) {
+      numbers.push(i);
+    }
   }
 
   return numbers;
@@ -19,3 +26,5 @@ generateButton.onclick = () => {
 
   possibles.splice(randomIndex, 1);
 }
+
+
